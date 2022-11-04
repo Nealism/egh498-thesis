0f1ef272d00b8767912998264d05b802884f7191
Brendan Tidd <brendan.tidd@data61.csiro.au>
2022-11-04 16:29:56 +1000

    Python 3.11 on hpc

--- bashies/base_csiro_pet.sh
+++ bashies/base_csiro_pet.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 sbatch <<EOT
 #!/bin/bash
+#SBATCH --OD-227199
 #SBATCH --job-name=$2
 #SBATCH --time=$4
 #SBATCH --mem=512g
 #SBATCH --ntasks-per-node=64
 #SBATCH --nodes=1
 ulimit -s 10240
-module load openmpi/4.1.2-ofed51-simple python/3.9.4 glew/2.2.0 mesa/21.1.0 patchelf/0.14.3
+module load openmpi/4.1.2-ofed51-simple python/3.11.0
 cd $HOME/behaviour_rl
 echo python $1 --exp $2 $3
 python $1 --exp $2 $3
--- bashies/multi_experiment.sh
+++ bashies/multi_experiment.sh
@@ -4,27 +4,25 @@
 # Experiment type
 # ==================================================================================
 declare -a Experiments=(
-                        "exp7/pos"
-                        "exp7/pos2"
-                        "exp7/pos_cur"
-                        "exp7/pos_cur2"
-                        "exp7/tor"
-                        "exp7/tor2"
-                        "exp7/tor_cur"
-                        "exp7/tor_cur2"
+                        "exp1/pos_py311"
+                        "exp1/tor_py311"
+                        "exp1/tor_grass_py311"
+                        "exp1/tor_tree_py311"
+                        "exp1/titan_grass_py311"
+                        "exp1/any_cur_py311"
+                        "exp1/any_py311"
                         )
 declare -a Arguments=(
                       # --training_on_hpc argument is required for mujoco environments 
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type position --training_on_hpc --seed 42"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type position --training_on_hpc --seed 84"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type position --training_on_hpc --cur --seed 42"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type position --training_on_hpc --cur --seed 84"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type torque --training_on_hpc --seed 42"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type torque --training_on_hpc --seed 84"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type torque --training_on_hpc --cur --seed 42"
-                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --control_type torque --training_on_hpc --cur --seed 84"
+                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --cur --control_type position --training_on_hpc"
+                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --cur --control_type torque --training_on_hpc"
+                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --cur --tree_type grass --control_type torque --training_on_hpc"
+                      "--cpu 64 --ident $USER --env franka_reach_dm_mj --cur --tree_type tree --control_type torque --training_on_hpc"
+                      "--cpu 64 --ident $USER --env titan_mj --training_on_hpc --tree_type grass"
+                      "--cpu 64 --ident $USER --env anymal_mj --cur --training_on_hpc"
+                      "--cpu 64 --ident $USER --env anymal_mj --training_on_hpc"
                       )
 
 for (( i=0; i<${#Arguments[@]}; i++ )); do 
-  sbatch ./base_csiro_pet.sh "run.py" ${Experiments[$i]} "${Arguments[$i]}" "12:00:00"
+  sbatch ./base_csiro_pet3_11.sh "run.py" ${Experiments[$i]} "${Arguments[$i]}" "12:00:00"
 done
