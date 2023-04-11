52fff625518da6b969944c56caaec7f0af6bf763
Brendan Tidd <brendan.tidd@data61.csiro.au>
2023-04-11 15:51:53 +1000

    Repair hpc bug in master

--- bashies/base_csiro_pet.sh
+++ bashies/base_csiro_pet.sh
@@ -2,17 +2,15 @@
 sbatch <<EOT
 #!/bin/bash
 #SBATCH --job-name=$2
-#SBATCH --output=o/$2.out
-#SBATCH --error=e/$2.err
 #SBATCH --time=$4
-#SBATCH --mem=32g
+#SBATCH --mem=500g
 #SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
 ulimit -s 10240
 module load openmpi/4.1.2-ofed51-simple python/3.11.0
 cd $HOME/behaviour_rl
 echo python $1 --exp $2 $3
-python $1 --exp $2 $3 
+python $1 --exp $2 $3
 
 hostname
 
