05326b257de469e44662c89df8c648e9e78c12a6
Md Mostafizur Rahman Komol <komol@data61.csiro.au>
2024-09-07 21:52:41 +1000

    Scene 1 clean map

--- assets/env_multi_robot_pb.py
+++ assets/env_multi_robot_pb.py
@@ -131,9 +131,9 @@ class Env(EnvBasePB):
             self.im_size = [1,self.local_map.shape[0],self.local_map.shape[1]]
             # print("Im",self.im_size,"local",self.local_map.shape);exit()
     def reset(self):
-        res = []
-        self.noise=np.random.choice([0.0,0.3])
-        # self.noise=0.0
+        #res = []
+        # self.noise=np.random.choice([0.0,0.3])
+        self.noise=0.0
         self.obstacles=[]
         self.robots_bbox=[]
         self.robots_pos=[]
